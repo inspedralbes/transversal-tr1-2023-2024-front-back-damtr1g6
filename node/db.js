3be73431f9f08e8d5b4ac0d57b079b8e485d01d9
a22jhepincre <a22jhepincre@inspedralbes.cat>
2023-10-23 11:42:01 +0200

    23/10/2023
    DB creada.

---
+++ node/db.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql2');
+const fs = require('fs');
+const date = new Date();
+
+var con = mysql.createConnection({
+    host: "dam.inspedralbes.cat",
+    user: "a22jhepincre_PR1",
+    password: "TR1Tienda",
+    database: "a22jhepincre_PR1Tienda"
+})
+
+function conectarBD() {
+    con.connect(function (err) {
+        if (err) {
+            console.log("No conexio");
+        } else {
+            console.log("Conectado");
+        }
+    })
+}
+
+function cerrarConexion() {
+    con.end(function (err) {
+        if (err) {
+            return console.log("error: " + err.message);
+        }
+        console.log("Se cierra la coneccion.");
+    })
+}
+
+conectarBD()
+cerrarConexion()
