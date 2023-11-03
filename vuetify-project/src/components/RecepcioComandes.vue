6e94d1e83f999ca18e4bb159de32e59601fd1134
damiabreacervino <113421275+damiabreacervino@users.noreply.github.com>
2023-11-03 09:15:58 +0100

    Revert "Extraer quantitat"
    This reverts commit 30616dbeb51294d450e7440f4579f31088ca53b2.

--- vuetify-project/src/components/RecepcioComandes.vue
+++ vuetify-project/src/components/RecepcioComandes.vue
@@ -88,7 +88,7 @@ export default {
                                     <v-row>
                                         <v-col cols="6">{{ extractNom(producto.nombre) }} </v-col>
                                         <v-col cols="3">{{ producto.precio }} €</v-col>
-                                        <v-col cols="3">{{ extraerValorEntreParentesis(producto.nombre) }} </v-col>
+                                        <v-col cols="3">{{ producto.nombre[1] }} </v-col>
                                     </v-row>
 
                                 </v-card-text>
